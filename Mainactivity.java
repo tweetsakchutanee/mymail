00c5cb10780cbcffb44294df6601801c68b26a1a
Tweetsak <84265277+tweetsakchutanee@users.noreply.github.com>
2021-06-16 14:16:26 +0700

    Create Mainactivity.java

---
+++ Mainactivity.java
@@ -0,0 +1,23 @@
+package com.my.mail;
+
+import android.app.*;
+
+import android.os.*;
+
+public class MainActivity extends Activity 
+
+{
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState)
+
+    {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+
+    }
+
+}
